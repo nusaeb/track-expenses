5e30982e7ccb4a25dba89c9848398cbe438c258d
Nusaeb Nur Alam <nusaeb@hotmail.com>
2019-10-24 13:39:10 +0300

    Added Eureka discovery server application

---
+++ discovery-server/src/test/java/com/trackexpenses/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackexpenses.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
