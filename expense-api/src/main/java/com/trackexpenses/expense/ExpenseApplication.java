73b10ea8dbc2739effa975eb05d415d92fa096e7
Nusaeb Nur Alam <nusaeb@hotmail.com>
2019-10-24 13:57:02 +0300

    Made category-api, expense-api and user-api discoverable as Eureka client

--- expense-api/src/main/java/com/trackexpenses/expense/ExpenseApplication.java
+++ expense-api/src/main/java/com/trackexpenses/expense/ExpenseApplication.java
@@ -2,8 +2,10 @@ package com.trackexpenses.expense;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ExpenseApplication {
 
 	public static void main(String[] args) {
